0fb44da405da39760108c1a4221142173d43b755
Arvid Nilsson <arvid.nilsson2@elev.ga.ntig.se>
2023-01-18 15:32:46 +0100

    adding room movement

---
+++ main.py
@@ -0,0 +1,11 @@
+room = 0
+
+while True:
+    if room == 0:
+        startRoom0()
+    elif room == 1:
+        startRoom1()
+    elif room == 2:
+        startRoom2()
+    elif room == 3:
+        startRoom3()
