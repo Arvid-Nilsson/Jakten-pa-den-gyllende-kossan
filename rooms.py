e8bb258bdc4c281e4086bb81c29d6d5afbdbc8e4
Seyed Reza Gowharvar Haghighi <reza.gowharvarhaghighi@elev.ga.ntig.se>
2023-02-07 20:00:10 +0100

    Finished toilet room for now.

--- rooms.py
+++ rooms.py
@@ -13,11 +13,11 @@ def Guldtoalett():
     
     while True:
     
-        Decision = Choices("Du är i Guldtoalleten, vad vill du göra?", ["gå till ett annat rum", "kolla runt"])
+        Decision = Choices("\nDu är i Guldtoalleten, vad vill du göra?", ["gå till ett annat rum", "kolla runt"])
         
         if Decision == 0:
             
-            Decision = Choices("Du tänker gå till ett annan rum, vilken?", ["gå tillbaka till hallen", "ångra dig"])
+            Decision = Choices("\nDu tänker gå till ett annan rum, vilken?", ["gå tillbaka till hallen", "ångra dig"])
 
             if Decision == 0:
 
@@ -25,7 +25,18 @@ def Guldtoalett():
             
             elif Decision == 1:
 
-                break
+                Decision = None
+
+        if Decision == 1: 
+
+            InventCheck = CheckBucket()
+            
+            if InventCheck == False:
+                GiveBucket()
+                print("\nDu plockade upp en hink!")
+            else:
+                print("\nDu hittade inte något nytt.")
+
 
 def Slutrum():
     NextRoom = input("du är i Slutrumet vilket rum vill du gå till?")
